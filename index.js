97eeb51fb97b98b76e36203428c41f736a815c7b
Samuel Attard <samuel.r.attard@gmail.com>
2018-11-20 04:46:25 +1100

    fix: only remove file not entire out directory

--- index.js
+++ index.js
@@ -2,25 +2,31 @@ var fs = require('fs-extra');
 var path = require('path');
 var run = require('electron-installer-run');
 var zipFolder = require('zip-folder');
-var format = require('util').format;
 var series = require('async').series;
 var debug = require('debug')('electron-installer-zip');
 
-module.exports = function(opts, done) {
+module.exports = function(_opts, done) {
+  var opts = Object.assign({}, _opts);
   opts.dir = path.resolve(opts.dir);
   opts.out = path.resolve(opts.out);
   opts.platform = opts.platform || process.platform;
+  if (path.extname(opts.out).toLowerCase() === '.zip') {
+    opts.outPath = opts.out;
+    opts.out = path.dirname(opts.out);
+  } else {
+    opts.outPath = path.resolve(opts.out, path.basename(opts.dir, '.app')) + '.zip';
+  }
 
   function zip(cb) {
     if (opts.platform !== 'darwin') {
-      zipFolder(opts.dir, opts.out, cb);
+      zipFolder(opts.dir, opts.outPath, cb);
       return;
     }
 
     var args = [
       '-r',
       '--symlinks',
-      opts.out,
+      opts.outPath,
       './'
     ];
 
@@ -28,19 +34,28 @@ module.exports = function(opts, done) {
       if (_err) {
         return cb(_err);
       }
-      cb(null, opts.dest);
+      cb(null, opts.outPath);
     });
   }
   debug('creating zip', opts);
 
   series([
-    fs.remove.bind(null, opts.out),
-    fs.mkdirs.bind(null, path.dirname(opts.out)),
-    zip,
+    function removeZipIfExists(cb) {
+      fs.stat(opts.outPath, function(err, stats) {
+        if (err) return cb(null);
+
+        if (!stats.isFile()) {
+          return cb(new Error('Refusing to wipe path "' + opts.outPath + '" as it is ' + (stats.isDirectory() ? 'a directory' : 'not a file')));
+        }
+        return fs.unlink(opts.outPath, cb);
+      });
+    },
+    fs.mkdirs.bind(null, opts.out),
+    zip
   ], function(err) {
     if (err) {
       return done(err);
     }
-    done(null, opts.dest);
+    done(null, opts.outPath);
   });
 };
