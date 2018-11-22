da3e48c22f0397af7eec0fff0ef969caf9a67f8a
Mark Lee <electronjs@lazymalevolence.com>
2018-11-21 23:35:34 -0800

    Promisify, ES6ify, and convert to cross-zip

--- index.js
+++ index.js
@@ -1,42 +1,36 @@
-var crossZip = require('cross-zip');
-var fs = require('fs-extra');
-var path = require('path');
-var series = require('async').series;
-var debug = require('debug')('electron-installer-zip');
+'use strict';
 
-module.exports = function (_opts, done) {
-  var opts = Object.assign({}, _opts);
+const debug = require('debug')('electron-installer-zip');
+const fs = require('fs-extra');
+const path = require('path');
+const pify = require('pify');
+const { zip } = require('cross-zip');
+
+function removeZipIfExists (opts) {
+  return fs.stat(opts.outPath)
+    .then(stats => {
+      if (!stats.isFile()) {
+        throw new Error(`Refusing to wipe path "${opts.outPath}" as it is ${stats.isDirectory() ? 'a directory' : 'not a file'}`);
+      }
+      return fs.unlink(opts.outPath);
+    });
+}
+
+module.exports = (userOpts, done) => {
+  const opts = Object.assign({}, userOpts);
   opts.dir = path.resolve(opts.dir);
   opts.out = path.resolve(opts.out);
-  opts.platform = opts.platform || process.platform;
   if (path.extname(opts.out).toLowerCase() === '.zip') {
     opts.outPath = opts.out;
     opts.out = path.dirname(opts.out);
   } else {
-    opts.outPath = path.resolve(opts.out, path.basename(opts.dir, '.app')) + '.zip';
+    opts.outPath = `${path.resolve(opts.out, path.basename(opts.dir, '.app'))}.zip`;
   }
 
   debug('creating zip', opts);
 
-  series([
-    function removeZipIfExists (cb) {
-      fs.stat(opts.outPath, function (err, stats) {
-        if (err) return cb(null);
-
-        if (!stats.isFile()) {
-          return cb(new Error('Refusing to wipe path "' + opts.outPath + '" as it is ' + (stats.isDirectory() ? 'a directory' : 'not a file')));
-        }
-        return fs.unlink(opts.outPath, cb);
-      });
-    },
-    fs.mkdirs.bind(null, opts.out),
-    function zip (cb) {
-      crossZip.zip(opts.dir, opts.outPath, cb);
-    }
-  ], function (err) {
-    if (err) {
-      return done(err);
-    }
-    done(null, opts.outPath);
-  });
+  return removeZipIfExists(opts)
+    .then(() => fs.mkdirs(opts.out))
+    .then(() => pify(zip)(opts.dir, opts.outPath))
+    .then(() => opts.outPath);
 };
--- test/index.test.js
+++ test/index.test.js
@@ -1,65 +1,50 @@
-var proxyquire = require('proxyquire');
-var assert = require('assert');
-var path = require('path');
-var fs = require('fs-extra');
-var sinon = require('sinon');
+'use strict';
 
-describe('electron-installer-zip', function () {
-  it('should be requireable', function () {
-    assert(require('../'));
-  });
+const assert = require('assert');
+const path = require('path');
+const fs = require('fs-extra');
+const sinon = require('sinon');
+const zip = require('..');
 
-  it('should refuse to wipe a directory', function (done) {
-    var options = {
+describe('electron-installer-zip', () => {
+  it('should refuse to wipe a directory', () => {
+    const options = {
       dir: path.join(__dirname, '..', 'bin'),
       out: path.join(__dirname, 'fixtures', 'bad.zip')
     };
 
-    var zip = proxyquire('../', {});
-    zip(options, function (err, outPath) {
-      assert.notStrictEqual(err, null);
-      assert.strictEqual(outPath, undefined);
-      assert.ok(err.message.includes('is a directory'));
-      done();
-    });
+    return zip(options)
+      .then(outPath => assert.fail('zip should not succeed'))
+      .catch(err => assert.ok(err.message.includes('is a directory')));
   });
 
-  it('should make a zip file in the out dir', function (done) {
-    var options = {
+  it('should make a zip file in the out dir', () => {
+    const options = {
       dir: path.join(__dirname, '..', 'bin'),
       out: path.join(__dirname, 'fixtures')
     };
 
-    fs.writeFile(options.out, 'content', function () {
-      var zip = proxyquire('../', {});
-      zip(options, function (err, outPath) {
-        assert.strictEqual(err, null);
+    return zip(options)
+      .then(outPath => {
         assert.strictEqual(outPath, path.resolve(options.out, 'bin.zip'));
-        fs.exists(outPath, function (exists) {
-          assert.ok(exists);
-          done();
-        });
-      });
-    });
+        return fs.pathExists(outPath);
+      }).then(exists => assert.ok(exists));
   });
 
-  it('should wipe the out file if it already exists', function (done) {
-    var options = {
+  it('should wipe the out file if it already exists', () => {
+    const options = {
       dir: path.join(__dirname, '..', 'bin'),
       out: path.join(__dirname, 'fixtures', 'good.zip')
     };
 
-    var unlinkSpy = sinon.spy(fs, 'unlink');
+    const unlinkSpy = sinon.spy(fs, 'unlink');
 
-    fs.writeFile(options.out, 'content', function () {
-      var zip = proxyquire('../', {});
-      zip(options, function (err, outPath) {
-        assert.strictEqual(err, null);
+    return fs.writeFile(options.out, 'content')
+      .then(() => zip(options))
+      .then(outPath => {
         assert.strictEqual(outPath, options.out);
         assert.strictEqual(unlinkSpy.callCount, 1);
-        assert.strictEqual(unlinkSpy.firstCall.args[0], options.out);
-        done();
+        return assert.strictEqual(unlinkSpy.firstCall.args[0], options.out);
       });
-    });
   });
 });
